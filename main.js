3f9522597e3655be2b9a90c4933374358ccb2d7b
Felipe Monsegur <felipemonsegur11@gmail.com>
2024-07-16 18:30:30 -0300

    4.0.0

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const DOMmostrarCarrito = document.getElementById('mostrar-carrito');
 
 const arayproductos = [{
     nombre: 'Banco dos cuerpos "Capataz"',
-    img: "./assets/img/banco dos cuerpos.jpg",
+    img: "./assets/img/Banco dos cuerpos “Capataz”.jpg",
     alt: "banco",
     descripcion: "Asiento 1.00 x 0.40 | Respaldo 0.50 | Altura Final 0.90",
     moneda: 0,
@@ -21,37 +21,248 @@ const arayproductos = [{
 },
 
 {
-    nombre: 'Silla matera “Lion”',
-    img: "./assets/img/Silla matera.jpg",
-    alt: "silla",
-    descripcion: "Largo 0.35 | Ancho 0.35 | Altura 0.65",
+    nombre: 'Banco dos cuerpos sin apoyabrazos “Capataz”',
+    img: "./assets/img/Banco dos cuerpos sin apoyabrazos “Capataz”.jpg",
+    alt: "banco",
+    descripcion: "Asiento 1.00 x 0.40 | Respaldo 0.50 | Altura Final 0.90",
     moneda: 0,
     id: 2,
 },
+
 {
-    nombre: 'Silla “Indio”',
-    img: "./assets/img/Silla.jpg",
-    alt: "silla",
-    descripcion: "Largo 0.30 | Ancho 0.65 | Altura 0.90",
+    nombre: 'Banco dos cuerpos “Paisano”',
+    img: "./assets/img/Banco dos cuerpos “Paisano”.jpg",
+    alt: "banco",
+    descripcion: "1.00 x 0.41 x 0.48 (Altura asiento) | 0.87 (Altura Final respaldo)",
     moneda: 0,
     id: 3,
 },
+
 {
-    nombre: 'Banco matero “Pionada”',
-    img: "./assets/img/Banco matero.jpg",
+    nombre: 'Banco un cuerpo “Patrón”',
+    img: "./assets/img/Banco un cuerpo “Patrón”.jpg",
     alt: "banco",
-    descripcion: "Largo 0.40 | Ancho 0.40 | Altura 0.45",
+    descripcion: "Largo 0.60 | Ancho 0.47 | Altura 0.90",
     moneda: 0,
     id: 4,
 },
+
 {
-    nombre: 'Mesa ratona “Rastra de oro”',
-    img: "./assets/img/Mesa ratona.jpg",
+    nombre: 'Mesa Ratona “Palenque”',
+    img: "./assets/img/Mesa Ratona “Palenque”.jpg",
     alt: "mesa",
     descripcion: "Largo 1.10 | Ancho 0.50 | Altura 0.45",
     moneda: 0,
     id: 5,
-}
+},
+
+{
+    nombre: 'Juego “Gallinales”',
+    img: "./assets/img/Juego “Gallinales”.jpg",
+    alt: "mesa",
+    descripcion: "Mesa: Largo 1.40 | Ancho 0.90 | Alto 0.68",
+    moneda: 0,
+    id: 6,
+},
+
+
+{
+    nombre: 'Mesa “Criolla”',
+    img: "./assets/img/Mesa “Criolla”.jpg",
+    alt: "mesa",
+    descripcion: "Se realiza a medida",
+    moneda: 0,
+    id: 7,
+},
+
+{
+    nombre: 'Recibidor “Puestero”',
+    img: "./assets/img/Recibidor “Puestero”.jpg",
+    alt: "mesa",
+    descripcion: "Largo 1.00 | Ancho 0.40 | Alto 0.75",
+    moneda: 0,
+    id: 8,
+},
+
+{
+    nombre: 'Mesa de arrime “Montura”',
+    img: "./assets/img/Mesa de arrime “Montura”.jpg",
+    alt: "mesa",
+    descripcion: "Largo 0.36 | Ancho 0.36 | Alto 0.74",
+    moneda: 0,
+    id: 9,
+},
+
+{
+    nombre: 'Pie de cama “Pilchas”',
+    img: "./assets/img/Pie de cama “Pilchas”.jpg",
+    alt: "mesa",
+    descripcion: "Largo 1.10 | Ancho 0.37 | Alto 0.45",
+    moneda: 0,
+    id: 10,
+},
+
+{
+    nombre: 'Silla “Indio”',
+    img: "./assets/img/Silla “Indio”.jpg",
+    alt: "silla",
+    descripcion: "Largo 0.30 | Ancho 0.65 | Alto 0.90",
+    moneda: 0,
+    id: 11,
+},
+
+{
+    nombre: 'Silla “Indio XL”',
+    img: "./assets/img/Silla “Indio XL”.jpg",
+    alt: "silla",
+    descripcion: "Largo 0.38 | Ancho 0.80 | Alto 1.05",
+    moneda: 0,
+    id: 12,
+},
+
+{
+    nombre: 'Silla matera “Lion”',
+    img: "./assets/img/Silla matera “Lion”.jpg",
+    alt: "silla",
+    descripcion: "Largo 0.35 | Ancho 0.35 | Altura asiento 0.30 | Altura respaldo 0.60",
+    moneda: 0,
+    id: 13,
+},
+
+{
+    nombre: 'Sillas “Tigre”',
+    img: "./assets/img/Sillas “Tigre”.jpg",
+    alt: "silla",
+    descripcion: "Largo 0.35 | Ancho 0.35 | Altura asiento 0.30 | Altura respaldo 0.60",
+    moneda: 0,
+    id: 14,
+},
+
+{
+    nombre: 'Sillas “Tigre XL”',
+    img: "./assets/img/Sillas “Tigre XL”.jpg",
+    alt: "silla",
+    descripcion: "Largo 0.35 | Ancho 0.35 | Altura asiento 0.45 | Altura respaldo 0.65",
+    moneda: 0,
+    id: 15,
+},
+
+{
+    nombre: 'Banco matero “Pionada”',
+    img: "./assets/img/Banco matero “Pionada”.jpg",
+    alt: "banco",
+    descripcion: "Largo 0.40 | Ancho 0.40 | Altura 0.45",
+    moneda: 0,
+    id: 16,
+},
+
+{
+    nombre: 'Banco “Gaucho”',
+    img: "./assets/img/Banco “Gaucho”.jpg",
+    alt: "banco",
+    descripcion: "Largo 0.35 | Ancho 0.35 | Altura 0.45",
+    moneda: 0,
+    id: 17,
+},
+
+{
+    nombre: 'Banco “Forastero”',
+    img: "./assets/img/Banco “Forastero”.jpg",
+    alt: "banco",
+    descripcion: "Largo 0.40 | Ancho 0.40 | Altura 0.45",
+    moneda: 0,
+    id: 18,
+},
+
+{
+    nombre: 'Cuerito Negro',
+    img: "./assets/img/Cuerito Negro.jpg",
+    alt: "cuero",
+    descripcion: "",
+    moneda: 0,
+    id: 19,
+},
+
+{
+    nombre: 'Cuerito Blanco',
+    img: "./assets/img/Cuerito Blanco.jpg",
+    alt: "cuero",
+    descripcion: "",
+    moneda: 0,
+    id: 20,
+},
+
+{
+    nombre: 'Juego “Troya”',
+    img: "./assets/img/Juego “Troya”.jpg",
+    alt: "cuero",
+    descripcion: "Barra: Largo 1.10 | Ancho 0.50| Altura 1.00 \nBanquetas (X2): Largo 0.35 | Ancho 0.35 | Altura 0.75",
+    moneda: 0,
+    id: 21,
+},
+
+{
+    nombre: 'Banqueta “Troya”',
+    img: "./assets/img/Banqueta “Troya”.jpg",
+    alt: "banco",
+    descripcion: "Largo 0.35 | Ancho 0.35 | Altura 0.75",
+    moneda: 0,
+    id: 22,
+},
+
+{
+    nombre: 'Biblioteca “Bellaquea”',
+    img: "./assets/img/Banqueta “Troya”.jpg",
+    alt: "biblioteca",
+    descripcion: "Largo 1.00 | Ancho 0.30 | Altura 0.90",
+    moneda: 0,
+    id: 23,
+},
+
+{
+    nombre: 'Cava “Bota de Potro”',
+    img: "./assets/img/Cava “Bota de Potro”.jpg",
+    alt: "cava",
+    descripcion: "Largo 0.34 | Ancho 0.25 | Altura 0.50",
+    moneda: 0,
+    id: 24,
+},
+
+{
+    nombre: 'Arcos de futbol “Potrero”',
+    img: "./assets/img/Arcos de futbol “Potrero”.jpg",
+    alt: "arco",
+    descripcion: "",
+    moneda: 0,
+    id: 25,
+},
+
+{
+    nombre: 'Perchero “Arisco”',
+    img: "./assets/img/Perchero “Arisco”.jpg",
+    alt: "perchero",
+    descripcion: "Realizado con clavos recuperados de ferrocarril. A elección: pintados en negro u oxidados.",
+    moneda: 0,
+    id: 26,
+},
+
+{
+    nombre: 'Abrebotellas “Chapiau”',
+    img: "./assets/img/Abrebotellas “Chapiau”.jpg",
+    alt: "perchero",
+    descripcion: "",
+    moneda: 0,
+    id: 27,
+},
+
+{
+    nombre: 'Porta espiral “Sortija”',
+    img: "./assets/img/Porta espiral “Sortija”.jpg",
+    alt: "porta espiral",
+    descripcion: "",
+    moneda: 0,
+    id: 28,
+},
 
 ];
 
